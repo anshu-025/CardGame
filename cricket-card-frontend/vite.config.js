9d6f06f8b8bcf01accbcf0a9c387821028836bdf
anshu-025 <anshumaan2545@gmail.com>
2026-08-20 03:37:51 +0530

    Update vite.config.js

--- cricket-card-frontend/vite.config.js
+++ cricket-card-frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['cardgame-1-4rik.onrender.com'],
+  },
 })
